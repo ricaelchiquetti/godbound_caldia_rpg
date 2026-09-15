b07edeab9fe5d4844b91820c41742cf7af2ac547
Ricael Vinicius Chiquetti <ricaelchiquetti27@gmail.com>
2026-09-14 23:50:21 -0300

    chore: add comment for GitHub Pages configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/godbound_caldia_rpg/',
+  base: '/godbound_caldia_rpg/', // Essencial para o GitHub Pages achar os arquivos
 })
