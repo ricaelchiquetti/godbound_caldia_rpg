a955a4628a0fe2695eb97d777cea29f33176d2a3
Ricael Vinicius Chiquetti <ricaelchiquetti27@gmail.com>
2026-09-15 13:46:46 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   base: '/godbound_caldia_rpg/', // Essencial ter as barras antes e depois do nome do repo!
 })
