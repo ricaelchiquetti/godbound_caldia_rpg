9e8b09272f455eb600835085d8eb8e7282d4d28b
Ricael Vinicius Chiquetti <ricaelchiquetti27@gmail.com>
2026-09-14 23:49:16 -0300

    chore: update homepage and configure deployment for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para o GitHub Pages
+  base: '/godbound_caldia_rpg/',
 })
